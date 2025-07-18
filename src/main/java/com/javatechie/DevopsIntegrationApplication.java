e08cb14acb00ed77573d8f88b546408109715676
Raj9556 <rajkumarsekar184@gmail.com>
2025-07-19 01:39:16 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to Raj Devops ";
 	}
 
 	public static void main(String[] args) {
